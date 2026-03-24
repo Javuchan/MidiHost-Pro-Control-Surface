ed658db55beaa8521da3a67d360daf09e8f0ea0c
Javuchan <javierapalominoslizana@gmail.com>
2026-03-24 20:57:50 -0300

    Add files via upload

--- server.ts
+++ server.ts
@@ -1,1 +1,7 @@
-web: npm start
+{
+  "name": "MidiHost Pro Control Surface by Javuchan",
+  "description": "Bridge the gap between your mobile device and your digital audio workstation with a high-fidelity, browser-based control surface. Designed for precision and low-latency performance, this interface utilizes the industry-standard Mackie Control Universal (MCU) protocol to provide comprehensive remote DAW integration via mobile browsers.",
+  "requestFramePermissions": [
+    "microphone"
+  ]
+}
